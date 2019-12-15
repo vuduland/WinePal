bff62588225cb034bbc471827117037f9eda60d1
Marc <marcjfj@gmail.com>
2019-12-15 14:19:31 -0600

    generated models

---
+++ models/inventory.js
@@ -0,0 +1,48 @@
+/* jshint indent: 2 */
+
+module.exports = function(sequelize, DataTypes) {
+  return sequelize.define(
+    'inventory',
+    {
+      id: {
+        type: DataTypes.INTEGER(11),
+        allowNull: false,
+        primaryKey: true,
+        primaryKey: true,
+        comment: 'null',
+        autoIncrement: true,
+      },
+      user_id: {
+        type: DataTypes.INTEGER(11),
+        allowNull: false,
+        comment: 'null',
+        references: {
+          model: 'users',
+          key: 'id',
+        },
+      },
+      quantity: {
+        type: DataTypes.INTEGER(11),
+        allowNull: false,
+        comment: 'null',
+      },
+      wine: {
+        type: DataTypes.INTEGER(11),
+        allowNull: false,
+        comment: 'null',
+        references: {
+          model: 'wines',
+          key: 'id',
+        },
+      },
+      vendor: {
+        type: DataTypes.STRING(100),
+        allowNull: true,
+        comment: 'null',
+      },
+    },
+    {
+      tableName: 'inventory',
+    }
+  );
+};
---
+++ models/users.js
@@ -0,0 +1,30 @@
+/* jshint indent: 2 */
+
+module.exports = function(sequelize, DataTypes) {
+  return sequelize.define(
+    'users',
+    {
+      id: {
+        type: DataTypes.INTEGER(11),
+        allowNull: false,
+        primaryKey: true,
+        primaryKey: true,
+        comment: 'null',
+        autoIncrement: true,
+      },
+      name: {
+        type: DataTypes.STRING(100),
+        allowNull: false,
+        comment: 'null',
+      },
+      email: {
+        type: DataTypes.STRING(100),
+        allowNull: false,
+        comment: 'null',
+      },
+    },
+    {
+      tableName: 'users',
+    }
+  );
+};
