765c9ae60dd8a834208d406fdbe5e180af175132
Marc <marcjfj@gmail.com>
2019-12-15 16:04:37 -0600

    db up and running

--- config/config.js
+++ config/config.js
@@ -1,26 +1,25 @@
+require('dotenv').config();
+
 module.exports = {
   development: {
     username: process.env.DB_USER,
     password: process.env.DB_PASS,
-    database: 'wine_DB',
+    database: 'wine_db',
     host: process.env.DB_HOST,
     dialect: 'mysql',
-    operatorsAliases: false,
   },
   test: {
     username: process.env.DB_USER,
     password: process.env.DB_PASS,
-    database: 'wine_DB',
+    database: 'wine_db',
     host: process.env.DB_HOST,
     dialect: 'mysql',
-    operatorsAliases: false,
   },
   production: {
     username: process.env.DB_USER,
     password: process.env.DB_PASS,
-    database: 'wine_DB',
+    database: 'wine_db',
     host: process.env.DB_HOST,
     dialect: 'mysql',
-    operatorsAliases: false,
   },
 };
--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,17 @@ var express = require('express');
 
 var router = express.Router();
 
+var db = require('../models');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.get('/api/all-wines', (req, res, next) => {
+  db.wines.findAll({}).then(data => {
+    res.json(data);
+  });
 });
 
 module.exports = router;
