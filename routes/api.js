15ec056c87b505ca9de0edbf07064df46f36c4ea
Victor <53414263+vuduland@users.noreply.github.com>
2019-12-16 19:21:36 -0600

    made registration, login, and initialized scripts

--- routes/api.js
+++ routes/api.js
@@ -5,6 +5,27 @@ var router = express.Router();
 var db = require('../models');
 var passport = require('../config/passport');
 
+router.get('/all-wines', (req, res, next) => {
+  db.wines.findAll({}).then(data => {
+    res.json(data);
+  });
+});
+
+router.post('/add-wine', (req, res, next) => {
+  db.wines
+    .create({
+      vintage: 2004,
+      country: 'USA',
+      region: 'California',
+      varietal: 'merlot',
+      producer: 'Marcle',
+      ageability_index: 10,
+    })
+    .then(data => {
+      res.json(data);
+    });
+});
+
 router.get('/all-users', (req, res, next) => {
   db.users.findAll({}).then(data => {
     res.json(data);
@@ -18,7 +39,7 @@ router.post('/add-user', (req, res, next) => {
       email: req.body.email,
       password: req.body.password,
     })
-    .then(data => res.json(data));
+    .then(data => res.json(data)); // sequelize promise
 });
 
 router.post('/login', passport.authenticate('local'), (req, res, next) => {
--- routes/index.js
+++ routes/index.js
@@ -6,29 +6,8 @@ var db = require('../models');
 var passport = require('../config/passport');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
-});
-
-router.get('/api/all-wines', (req, res, next) => {
-  db.wines.findAll({}).then(data => {
-    res.json(data);
-  });
-});
-
-router.post('/api/add-wine', (req, res, next) => {
-  db.wines
-    .create({
-      vintage: 2004,
-      country: 'USA',
-      region: 'California',
-      varietal: 'merlot',
-      producer: 'Marcle',
-      ageability_index: 10,
-    })
-    .then(data => {
-      res.json(data);
-    });
 });
 
 router.get('/dashboard', (req, res, next) => {
@@ -40,6 +19,14 @@ router.get('/dashboard', (req, res, next) => {
       email: req.user.email,
     });
   }
+});
+
+router.get('/register', function (req, res, next) {
+  res.render('register', { title: 'Registration' });
+});
+
+router.get('/login', function (req, res, next) {
+  res.render('login', { title: 'Login' });
 });
 
 module.exports = router;
