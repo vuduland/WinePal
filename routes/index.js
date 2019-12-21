2a0d5b7a7f6be6c18c6d0a8fa64589e0d882b8d1
Peter Smith <petergbsmith@gmail.com>
2019-12-21 00:07:57 -0600

    add try/catch blocks; tabs; edit dashboard

--- routes/index.js
+++ routes/index.js
@@ -6,43 +6,51 @@ var db = require('../models');
 var passport = require('../config/passport');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: '🍷 Wine Pal 🍷' });
+router.get('/', function (req, res, next) {
+    res.render('index', { title: '🍷 Wine Pal 🍷' });
 });
 
 router.get('/dashboard', (req, res, next) => {
-  if (!req.user) {
-    res.json({});
-  } else {
-    db.Inventory.findAll({
-      where: {
-        userId: req.user.id,
-      },
-      include: [
-        {
-          model: db.Wine,
-          as: 'Wine',
-          required: true,
-        },
-      ],
-    }).then(userWines => {
-      console.log(userWines);
-      res.render('dashboard', {
-        name: req.user.name,
-        email: req.user.email,
-        id: req.user.id,
-        userWines,
-      });
-    });
-  }
+    if (!req.user) {
+        res.json({});
+    } else {
+        db.Inventory.findAll({
+            where: {
+                userId: req.user.id,
+            },
+            include: [
+                {
+                    model: db.Wine,
+                    as: 'Wine',
+                    required: true,
+                },
+            ],
+        }).then(userWines => {
+            console.log(userWines);
+            res.render('dashboard', {
+                name: req.user.name,
+                email: req.user.email,
+                id: req.user.id,
+                userWines,
+            });
+        });
+    }
 });
 
-router.get('/register', function(req, res, next) {
-  res.render('register', { title: 'Registration' });
+router.get('/register', function (req, res, next) {
+    try {
+        res.render('register', { title: 'Registration' });
+    } catch (err) {
+        console.log(err);
+    }
 });
 
-router.get('/login', function(req, res, next) {
-  res.render('login', { title: 'Login' });
+router.get('/login', function (req, res, next) {
+    try {
+        res.render('login', { title: 'Login' });
+    } catch (err) {
+        console.log(err);
+    }
 });
 
 module.exports = router;
