ff95f43f3d4acf0c7b868ac49d7016c0aeaa320b
Marc <marcjfj@gmail.com>
2019-12-30 20:04:45 -0600

    better looking notes

--- app.js
+++ app.js
@@ -16,6 +16,8 @@ require('dotenv').config();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
+app.locals.moment = require('moment');
+app.locals.moment = require('moment-timezone');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -40,12 +42,12 @@ app.use('/', indexRouter);
 app.use('/api', apiRouter);
 
 // catch 404 and forward to error handler
-app.use(function (req, res, next) {
+app.use(function(req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function (err, req, res, next) {
+app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
