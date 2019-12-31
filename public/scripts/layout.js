aeb7eb701ac37549bcf22b038941470f0369a15c
Marc <marcjfj@gmail.com>
2019-12-30 21:21:30 -0600

    got menu started

--- public/scripts/layout.js
+++ public/scripts/layout.js
@@ -1,1 +1,11 @@
 // if we have time we might switch the POST method used over to an ajax type dynamically generated javascript
+const menuButton = document.querySelector('.menu-button');
+const sideMenu = document.querySelector('.side-menu');
+menuButton.addEventListener('click', () => {
+  sideMenu.classList.remove('closed');
+});
+
+const closeButton = document.querySelector('.close-menu');
+closeButton.addEventListener('click', () => {
+  sideMenu.classList.add('closed');
+});
