5502a05c59a9b0c0212c66813504a87370cdab79
Marc <marcjfj@gmail.com>
2020-01-04 04:32:17 -0600

    added jaws

--- models/index.js
+++ models/index.js
@@ -8,18 +8,22 @@ const env = process.env.NODE_ENV || 'development';
 const config = require(`${__dirname}/../config/config.js`)[env];
 const db = {};
 
-const sequelize = new Sequelize(
-  config.database,
-  config.username,
-  config.password,
-  {
-    host: config.host,
-    dialect: 'mysql',
-    define: {
-      // timestamps: false,
-    },
-  }
-);
+if (config.use_env_variable) {
+  const sequelize = new Sequelize(precess.env[config.use_env_variable], config);
+} else {
+  const sequelize = new Sequelize(
+    config.database,
+    config.username,
+    config.password,
+    {
+      host: config.host,
+      dialect: 'mysql',
+      define: {
+        // timestamps: false,
+      },
+    }
+  );
+}
 
 fs.readdirSync(__dirname)
   .filter(
